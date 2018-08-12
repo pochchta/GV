52cee76f51ec30c3b382d9443e651a082d95c45d
pochchta <pochchta@rambler.ru>
2018-08-12 23:49:56 +0800

    time

--- js.js
+++ js.js
@@ -7,7 +7,6 @@
 // @include https://godville.net/superhero
 // ==/UserScript==
 
-
 var delay_counter = 0;
 var new_Date;
 //var last_id;
@@ -24,8 +23,20 @@ function change(){
 }
 */
 
+const health_set = 30, prana_set = 30;
+var arr = [
+    ["Время", "00:01:02", ""],
+    ["Создан", "00:01:02", ""],
+    ["Здоровье", "отсечка " + health_set, "0"],
+    ["Прана", "отсечка " + prana_set, "0"],
+    ["На арену", "возможность отправки", "0"]
+];
+
 function inic(){
     if ( ! document.getElementById("my_test_block") ) {
+        new_Date = new Date();
+        arr[1][1] = new_Date.getHours() + ":" + new_Date.getMinutes() + ":" + new_Date.getSeconds();
+
         var div_block = document.createElement('div'), div_block_content = document.createElement('div'), div_block_end_line = document.createElement('div');
         var div_equ = document.getElementById("equipment");
         var div_equ_block_h = div_equ.getElementsByClassName("block_h");
@@ -34,27 +45,41 @@ function inic(){
         div_block.className = "block";
         var div_block_h = div_equ_block_h[0].cloneNode(true);
         (div_block_h.getElementsByClassName("block_title"))[0].innerHTML = "My_test_block ))";
-
-        div_block_content.className = "block_content";
-        var div_block_line = div_equ_line[0].cloneNode(true);
-        div_block_line.id = "my_line_0";
-        ( div_block_line.getElementsByClassName("eq_name") )[0].innerHTML = "--------------------";
-        div_block_end_line.className = "line";
-
-        document.getElementById("left_block").insertBefore( div_block , document.getElementById("pet") );
         div_block.appendChild(div_block_h);
         div_block.appendChild(div_block_content);
-        div_block_content.appendChild(div_block_line);
-        div_block_content.appendChild(div_block_line.cloneNode(true));
+        div_block_content.className = "block_content";
+        div_block_end_line.className = "line";
+
+        var div_block_line = [];
+        arr.forEach(function(item, i, arr) {
+            div_block_line[i] = div_equ_line[0].cloneNode(true);
+            div_block_line[i].id = "my_line_" + i;
+            ( div_block_line[i].getElementsByClassName("eq_capt") )[0].innerHTML = item[0];
+            ( div_block_line[i].getElementsByClassName("eq_name") )[0].innerHTML = item[1];
+            ( div_block_line[i].getElementsByClassName("eq_level") )[0].innerHTML = item[2];
+            div_block_content.appendChild(div_block_line[i]);
+        });
+
         div_block_content.appendChild(div_block_end_line);
+        document.getElementById("left_block").insertBefore( div_block , document.getElementById("pet") );
+
     }
 }
 
 function loop(){
     inic();
-    new_Date = new Date();
 
-    //alert(new_Date.getSeconds());
+    new_Date = new Date();
+    arr[0][1] = new_Date.getHours() + ":" + new_Date.getMinutes() + ":" + new_Date.getSeconds();
+
+    var div_block_line;
+    arr.forEach(function(item, i, arr) {
+        div_block_line = document.getElementById("my_line_" + i);
+        ( div_block_line.getElementsByClassName("eq_name") )[0].innerHTML = item[1];
+        ( div_block_line.getElementsByClassName("eq_level") )[0].innerHTML = item[2];
+    });
+
+
     delay_counter++;
     if (delay_counter > DELAY_SET) delay_counter = 0;
 }
